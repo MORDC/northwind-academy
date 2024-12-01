ed8fcfaea7cac75f6e7f1897cc9a560a8f47bf27
Marcos Andre Santos <marcos.santos@indicium.tech>
2024-12-01 22:01:07 +0000

    documentação e configurações globais

--- models/intermediate/int_vendas_prep_produtos.sql
+++ models/intermediate/int_vendas_prep_produtos.sql
@@ -1,34 +1,37 @@
 with
+    /* Chamada dos modelos necessários. */
     categorias as (
         select *
         from {{ ref('stg_erp_categorias') }}
     )
-   ,fornecedores as (
+
+    , fornecedores as (
         select *
         from {{ ref('stg_erp_fornecedores') }}
     )
-    ,produtos as (
+
+    , produtos as (
         select *
         from {{ ref('stg_erp_produtos') }}
     )
 
     , enriquecer_produtos as (
         select
-             produtos.PK_PRODUTO
-            ,produtos.NM_PRODUTO
-            ,produtos.QUANTIDADE_POR_UNIDADE
-            ,produtos.PRECO_POR_UNIDADE
-            ,produtos.UNIDADE_EM_ESTOQUE
-            ,produtos.UNIDADE_POR_PEDIDO
-            ,produtos.NIVEL_DE_PEDIDO
-            ,produtos.EH_DISCONTINUADO
-            ,categorias.nome_categoria
-            ,fornecedores.nome_fornecedor
-            ,fornecedores.cidade_fornecedor
-            ,fornecedores.pais_fornecedor
+            produtos.PK_PRODUTO
+            , produtos.NM_PRODUTO
+            , produtos.QUANTIDADE_POR_UNIDADE
+            , produtos.PRECO_POR_UNIDADE
+            , produtos.UNIDADE_EM_ESTOQUE
+            , produtos.UNIDADE_POR_PEDIDO
+            , produtos.NIVEL_DE_PEDIDO
+            , produtos.EH_DISCONTINUADO
+            , categorias.NOME_CATEGORIA
+            , fornecedores.NOME_FORNECEDOR
+            , fornecedores.CIDADE_FORNECEDOR
+            , fornecedores.PAIS_FORNECEDOR 
         from produtos
-        left join categorias on produtos.fk_categoria=categorias.pk_categoria
-        left join fornecedores on produtos.fk_fornecedor=fornecedores.pk_fornecedor
+        left join categorias on produtos.fk_categoria = categorias.pk_categoria
+        left join fornecedores on produtos.fk_fornecedor = fornecedores.pk_fornecedor
     )
 
 select *
